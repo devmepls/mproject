41460078664a78c51594f5e7635ebb6e8e6d7d3b
Vishal Kumar <vishal.kr12536@gmail.com>
2023-03-07 16:23:28 +0530

    latest

---
+++ server/src/ai.py
@@ -0,0 +1,27 @@
+from fastai.vision import *
+import torch
+import torchvision
+from fastbook import load_learner
+from fastai.vision.all import *
+import pandas as pd
+import numpy as np
+import PIL
+from PIL import Image
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+from pathlib import Path
+
+
+
+
+
+def test(imgpath, trainpath):
+    img = load_image(imgpath)
+    path2 = Path(trainpath)
+    learn = load_learner(path2,'export.pkl')
+    pred, idx, outputs = learn.predict(img)
+    print('Predicted class: ', pred)
+    return pred
+
+
